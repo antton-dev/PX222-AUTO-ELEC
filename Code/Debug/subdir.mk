c94cae48bca7bdee2ac0204fd9f1f7bf6ea811c3
Antton <antton.chevrier@etik.com>
2026-06-02 14:56:58 +0200

    pwm

---
+++ Code/Debug/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c \
+../syscalls.c \
+../sysmem.c 
+
+C_DEPS += \
+./main.d \
+./syscalls.d \
+./sysmem.d 
+
+OBJS += \
+./main.o \
+./syscalls.o \
+./sysmem.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./main.d ./main.o ./syscalls.d ./syscalls.o ./sysmem.d ./sysmem.o
+
+.PHONY: clean--2e-
+
